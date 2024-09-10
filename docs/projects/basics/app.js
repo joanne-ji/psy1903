48e9d13d5cbaf935a7b5f8f1876a97b620d01307
Jae Ji <jj2joanne@gmail.com>
2024-09-10 02:03:32 -0400

    Week 2 practice code from videos.

--- docs/projects/basics/app.js
+++ docs/projects/basics/app.js
@@ -1,1 +1,32 @@
-console.log('Hello World');
+let welcomeMessage = `
+Welcome to our experiment!
+Please read the instructions carefully.
+`;
+
+let experimentName = 'Stroop';
+console.log(typeof experiment); // string\
+console.log(experiment.charAt(1)); // 't'
+
+let trialCount = 20;
+console.log(typeof trialCountMax); // number
+
+// TODO: Randomize colors
+let colors = ['red', 'green', 'blue'];
+
+alert('Welcome to the ' + experimentName + ' experiment!');
+
+// trialCount = 40;
+
+// At the halfway point, we will display a pause screen
+let halfwayCount = trialCount / 2; // Expected: 20
+
+/*
+This can be a multi-line comment
+if need be
+like this!
+ */
+
+let correct = false;
+console.log(typeof correct);
+
+console.log(10 < 15); // false
