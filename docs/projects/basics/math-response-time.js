e1c0fb5553661e6a56e1c9b96d42efb8ed92cc26
Jae Ji <jj2joanne@gmail.com>
2024-09-10 17:39:37 -0400

    task set 2 math question update

--- docs/projects/basics/math-response-time.js
+++ docs/projects/basics/math-response-time.js
@@ -4,20 +4,27 @@ alert('In this experiment we will measure your response time. You will be shown 
 // Create three addition equations with whole numbers of your choosing between 1 and 10.
 // Collect time before and after each trial.
 // After each trial, the participant sees an alert saying "You answered [insert their answer here] in [response time] seconds."
+let randomNumber1 = Math.floor(Math.random() * 10) + 1;
+let randomNumber2 = Math.floor(Math.random() * 10) + 1;
+
 let start1 = Date.now();
-let response1 = prompt('What is 5+3?');
+let response1 = prompt('What is ' + randomNumber1 + ' + ' + randomNumber2 + '?');
 let end1 = Date.now();
 let responseTime1 = (end1 - start1) / 1000;
 alert('You answered ' + response1 + ' in ' + responseTime1 + ' seconds!');
 
+randomNumber1 = Math.floor(Math.random() * 10) + 1;
+randomNumber2 = Math.floor(Math.random() * 10) + 1;
 let start2 = Date.now();
-let response2 = prompt('What is 4+4?');
+let response2 = prompt('What is ' + randomNumber1 + ' + ' + randomNumber2 + '?');
 let end2 = Date.now();
 let responseTime2 = (end2 - start2) / 1000;
 alert('You answered ' + response2 + ' in ' + responseTime2 + ' seconds!');
 
+randomNumber1 = Math.floor(Math.random() * 10) + 1;
+randomNumber2 = Math.floor(Math.random() * 10) + 1;
 let start3 = Date.now();
-let response3 = prompt('What is 1+8?');
+let response3 = prompt('What is ' + randomNumber1 + ' + ' + randomNumber2 + '?');
 let end3 = Date.now();
 let responseTime3 = (end3 - start3) / 1000;
 alert('You answered ' + response3 + ' in ' + responseTime3 + ' seconds!');
