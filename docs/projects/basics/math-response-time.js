554ea379c94d8a7cbb8e5d249449a7d0b19dd08a
Jae Ji <jj2joanne@gmail.com>
2024-09-10 17:26:25 -0400

    task set 2 updated math response time question

--- docs/projects/basics/math-response-time.js
+++ docs/projects/basics/math-response-time.js
@@ -2,15 +2,25 @@
 alert('In this experiment we will measure your response time. You will be shown a series of simple math equations. Answer these equations as quickly and accurately as you canWelcome to our experiment!');
 
 // Create three addition equations with whole numbers of your choosing between 1 and 10.
-// After each equation, the participant sees an alert saying "You answered [insert their answer here]."
+// Collect time before and after each trial.
+// After each trial, the participant sees an alert saying "You answered [insert their answer here] in [response time] seconds."
+let start1 = Date.now();
 let response1 = prompt('What is 5+3?');
-alert('You answered ' + response1 + '!');
+let end1 = Date.now();
+let responseTime1 = (end1 - start1) / 1000;
+alert('You answered ' + response1 + ' in ' + responseTime1 + ' seconds!');
 
+let start2 = Date.now();
 let response2 = prompt('What is 4+4?');
-alert('You answered ' + response2 + '!');
+let end2 = Date.now();
+let responseTime2 = (end2 - start2) / 1000;
+alert('You answered ' + response2 + ' in ' + responseTime2 + ' seconds!');
 
+let start3 = Date.now();
 let response3 = prompt('What is 1+8?');
-alert('You answered ' + response3 + '!');
+let end3 = Date.now();
+let responseTime3 = (end3 - start3) / 1000;
+alert('You answered ' + response3 + ' in ' + responseTime3 + ' seconds!');
 
 // At the end, display an alert that says "Thank you for your participation!"
 alert('Thank you for your participation!');
