75030fa9968c9dc3551f621eb6d69ca1a5244ede
Jae Ji <jj2joanne@gmail.com>
2024-09-17 16:39:39 -0400

    week 3 task set update 1

--- docs/projects/basics/app.js
+++ docs/projects/basics/app.js
@@ -44,17 +44,17 @@ like this!
 // console.log('Hello World');
 // console.log("Hello World");
 
-let courseName = 'Programming for Psychologists';
-console.log(courseName.length); // Output: 29
+// let courseName = 'Programming for Psychologists';
+// console.log(courseName.length); // Output: 29
 
-// Output the value for courseName translated to all capital letters.
-// Expected result: PROGRAMMING FOR PSYCHOLOGISTS
-console.log(courseName.toUpperCase());
+// // Output the value for courseName translated to all capital letters.
+// // Expected result: PROGRAMMING FOR PSYCHOLOGISTS
+// console.log(courseName.toUpperCase());
 
-// Output the index position of the word 'Psychologists' within the string stored in courseName.
-// Expected result: 16
-console.log(courseName.indexOf('Psychologists'));
+// // Output the index position of the word 'Psychologists' within the string stored in courseName.
+// // Expected result: 16
+// console.log(courseName.indexOf('Psychologists'));
 
-// Output the value for courseName with the word 'Programming' replaced with the word 'Coding'.
-// Expected result: "Coding for Psychologists"
-console.log(updated = courseName.replace('Programming', 'Coding'));
+// // Output the value for courseName with the word 'Programming' replaced with the word 'Coding'.
+// // Expected result: "Coding for Psychologists"
+// console.log(updated = courseName.replace('Programming', 'Coding'));
