7ace64076346d9ec708fb358fd408b441e8508cb
Jae Ji <jj2joanne@gmail.com>
2024-09-17 17:22:59 -0400

    update

--- docs/projects/input-and-output/input-and-output-review/app.js
+++ docs/projects/input-and-output/input-and-output-review/app.js
@@ -1,7 +1,6 @@
 // Create variables to store references to elements on the page
 let form = document.getElementsByTagName('form')[0];
 let results = document.getElementById('results');
-let equation = document.getElementById('equation');
 
 let num1Output = document.getElementById('num1');
 let num2Output = document.getElementById('num2');
@@ -9,10 +8,8 @@ let num2Output = document.getElementById('num2');
 let num1 = Math.floor(Math.random() * 10) + 1;
 let num2 = Math.floor(Math.random() * 10) + 1;
 
-equation.innerHTML = 'What is ' + num1 + ' + ' + num2 + '?';
-
 num1Output.innerHTML = num1;
-num10inout.innerHTML = num2;
+num2Output.innerHTML = num2;
 
 // Listen for the form to be submitted
 form.addEventListener('submit', function (event) {
@@ -22,21 +19,15 @@ form.addEventListener('submit', function (event) {
 
     // Stop the timer!
 
-    // Collect the name
-    let response = form.elements['name'].value;
+    // Calculate response time
 
-    let resultsMessage = '';
-
-    if (over18) {
-        resultsMessage = 'Hello ' + response + '!';
-    } else {
-        resultsMessage = 'This experiment is only for those older than 18.';
-    }
+    // Collect the response
+    let response = form.elements['response'].value;
 
     // Report the results
-    results.innerHTML = resultsMessage;
+    results.innerHTML = 'Hello ' + response + '!';
 
-    // Hide the form, including the instructions
+    // Hide the form (including instructions)
     form.style.display = 'none';
 
 });
