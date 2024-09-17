5f4ce29120f599309e39c20b794c1a77ee8040d8
Jae Ji <jj2joanne@gmail.com>
2024-09-17 15:15:07 -0400

    update

--- docs/projects/basics/app.js
+++ docs/projects/basics/app.js
@@ -49,12 +49,12 @@ console.log(courseName.length); // Output: 29
 
 // Output the value for courseName translated to all capital letters.
 // Expected result: PROGRAMMING FOR PSYCHOLOGISTS
-console.log([courseName.toUpperCase()]);
+console.log(courseName.toUpperCase());
 
 // Output the index position of the word 'Psychologists' within the string stored in courseName.
 // Expected result: 16
-console.log([courseName.indexOf('Psychologists')]);
+console.log(courseName.indexOf('Psychologists'));
 
 // Output the value for courseName with the word 'Programming' replaced with the word 'Coding'.
 // Expected result: "Coding for Psychologists"
-console.log([updated = courseName.replace('Programming', 'Coding')]);
+console.log(updated = courseName.replace('Programming', 'Coding'));
