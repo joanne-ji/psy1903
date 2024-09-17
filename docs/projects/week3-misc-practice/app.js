a348e95cd09053a8362c30efd8980dc384279ba0
Jae Ji <jj2joanne@gmail.com>
2024-09-17 16:55:20 -0400

    week 3 task set misc practice part b

--- docs/projects/week3-misc-practice/app.js
+++ docs/projects/week3-misc-practice/app.js
@@ -1,3 +1,5 @@
+// Misc Practice Part A
+
 let randomNumber1 = Math.floor(Math.random() * 10) + 1;
 let randomNumber2 = Math.floor(Math.random() * 10) + 1;
 
@@ -16,3 +18,16 @@ if (response == randomNumber1 + randomNumber2) {
 }
 
 alert(feedback + ' The expected answer is ' + correctAnswer + '.');
+
+// Misc Practice Part B
+
+let age = prompt('How old are you?');
+if (age < 12) {
+    alert('Child');
+}
+if (age >= 12 && age <= 17) {
+    alert('Teenager');
+}
+if (age >= 18) {
+    alert('Adult');
+}
