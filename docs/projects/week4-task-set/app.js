248923ee29337ae47bddae8c7b8aee43b976068a
Jae Ji <jj2joanne@gmail.com>
2024-09-24 17:41:51 -0400

    week 4 task set question 5

--- docs/projects/week4-task-set/app.js
+++ docs/projects/week4-task-set/app.js
@@ -6,17 +6,29 @@
 
 // console.log(celsiusToFahrenheit(10)); // Expected output: 50
 
-// Question 4
-function convertTemp(temp, convertTo) {
-    if (convertTo == 'c') {
-        let celsius = (temp - 32) / 1.8;
-        return celsius;
-    } else {
-        let fahrenheit = (temp * 1.8) + 32;
-        return fahrenheit;
+// // Question 4
+// function convertTemp(temp, convertTo) {
+//     if (convertTo == 'c') {
+//         let celsius = (temp - 32) / 1.8;
+//         return celsius;
+//     } else {
+//         let fahrenheit = (temp * 1.8) + 32;
+//         return fahrenheit;
+//     }
+// }
+
+// console.log(convertTemp(10, 'c')); // Expected output: -12.222222222222221
+// console.log(convertTemp(10, 'f')); // Expected output: 50
+
+// Question 5
+
+function getWordLengths(words) {
+    let wordLengths = [];
+    for (let word of words) {
+        wordLengths.push(word.length);
     }
+    return wordLengths;
 }
 
-console.log(convertTemp(10, 'c')); // Expected output: -12.222222222222221
-console.log(convertTemp(10, 'f')); // Expected output: 50
-
+let words = ['apple', 'banana', 'cherry', 'pear', 'grape'];
+console.log(getWordLengths(words)); // Expected output: [5, 6, 6, 4, 5]
