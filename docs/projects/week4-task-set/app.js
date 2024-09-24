abc38a65f5a29eef9582a8e8192057712a5af23e
Jae Ji <jj2joanne@gmail.com>
2024-09-24 17:16:46 -0400

    week 4 task set question 4

--- docs/projects/week4-task-set/app.js
+++ docs/projects/week4-task-set/app.js
@@ -1,7 +1,21 @@
-// Question 3
-function celsiusToFahrenheit(celsius) {
-    let farenheit = (celsius * 1.8) + 32;
-    return farenheit;
+// // Question 3
+// function celsiusToFahrenheit(celsius) {
+//     let farenheit = (celsius * 1.8) + 32;
+//     return farenheit;
+// }
+
+// console.log(celsiusToFahrenheit(10)); // Expected output: 50
+
+// Question 4
+function convertTemp(temp, convertTo) {
+    if (convertTo == 'c') {
+        let celsius = (temp - 32) / 1.8;
+        return celsius;
+    } else {
+        let fahrenheit = (temp * 1.8) + 32;
+        return fahrenheit;
+    }
 }
 
-console.log(celsiusToFahrenheit(10)); // Expected output: 50
+console.log(convertTemp(10, 'c')); // Expected output: -12.222222222222221
+console.log(convertTemp(10, 'f')); // Expected output: 50
