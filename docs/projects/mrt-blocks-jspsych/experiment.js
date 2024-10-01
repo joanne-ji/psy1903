4090957a336e112dfaee03d48e317903fbfe8834
Jae Ji <jj2joanne@gmail.com>
2024-10-01 17:41:39 -0400

    week 5 task set question 5

---
+++ docs/projects/mrt-blocks-jspsych/experiment.js
@@ -0,0 +1,79 @@
+let jsPsych = initJsPsych();
+
+let timeline = [];
+
+// Welcome
+let welcomeTrial = {
+    type: jsPsychHtmlKeyboardResponse,
+    stimulus: `
+    <h1>Welcome to the Math Response Time Task!</h1> 
+    <p>In this experiment, you will be shown a series of math expressions.</p>
+    <p>Please answer as quickly and as accurately as possible.</p>
+    <p>Press SPACE to begin.</p>
+    `,
+    choices: [' '],
+};
+
+timeline.push(welcomeTrial);
+
+// Conditions
+for (let block of conditions) {
+    let blockIntroTrial = {
+        type: jsPsychHtmlKeyboardResponse,
+        stimulus: `
+        <h1>${block.title}</h1>
+        <p>Press SPACE to begin.</p>
+        `,
+        choices: [' '],
+    }
+    timeline.push(blockIntroTrial);
+
+    for (let conditionTrial of block.questions) {
+        let trial = {
+            type: jsPsychSurveyHtmlForm,
+            preamble: 'What is ' + conditionTrial.num1 + '+' + conditionTrial.num2 + ' ?',
+            html: `
+            <input type='text' name='answer' id='answer'>
+            `,
+            autofocus: 'answer',
+            button_label: 'Submit',
+            data: {
+                collect: true,
+                num1: conditionTrial.num1,
+                num2: conditionTrial.num2,
+                block: block.title
+            },
+            on_finish: function (data) {
+                data.answer = data.response.answer;
+                if (data.answer == conditionTrial.answer) {
+                    data.correct = true;
+                } else {
+                    data.correct = false;
+                }
+            }
+        };
+        timeline.push(trial);
+    }
+}
+
+// Debrief
+let debriefTrial = {
+    type: jsPsychHtmlKeyboardResponse,
+    stimulus: `
+    <h1>Thank you for participating!</h1> 
+    <p>You can close this tab.</p>
+    `,
+    choices: ['NO KEYS'],
+    on_start: function () {
+        let data = jsPsych.data
+            .get()
+            .filter({ collect: true })
+            .ignore(['collect', 'stimulus', 'trial_type', 'trial_index', 'plugin_version'])
+            .csv();
+        console.log(data);
+    }
+};
+
+timeline.push(debriefTrial);
+
+jsPsych.run(timeline);
